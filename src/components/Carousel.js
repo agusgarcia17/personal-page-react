7c009ab1dad54035caa2c63b3754b4b3854e0923
agusgarcia17 <agus.garcia17@gmail.com>
2020-08-05 17:24:07 +0200

    Display de proyectos

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Card from './Card';
+import ADA from '../img/proyectoFinal-ADA.jpg';
+import PHP from '../img/Proyecto-PHP.jpg';
+import WES from '../img/Catch-of-the-day-React.jpg';
+import {Container, Row } from 'react-bootstrap';
 
 class Carousel extends React.Component {
 
@@ -6,14 +11,71 @@ class Carousel extends React.Component {
         super (props);
     
         this.state = {
+            items: [
+                {
+                id: 0,
+                title: 'Proyecto final de Bootcamp en ADA',
+                subTitle: 'Desarrollo en React y Node',
+                imgSrc: ADA,
+                link: 'https://github.com/agusgarcia17/proyecto-final',
+                selected: false
+            },
+            {
+                id: 1,
+                title: 'Proyecto final de curso de PHP',
+                subTitle: 'Desarrollo en PHP con Laravel y base de datos',
+                imgSrc: PHP,
+                link: 'https://obscure-refuge-50354.herokuapp.com/',
+                selected: false
+            },
+            {
+                id: 2,
+                title: 'Proyecto de curso de React',
+                subTitle: 'Desarrollo en React y Firebase segun curso de Wes Bos',
+                imgSrc: WES,
+                link: 'https://github.com/agusgarcia17/catch-of-the-day',
+                selected: false
+            },
+        ]
 
         }
     
     }
 
+
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items]
+
+        items[id].selected = items[id].selected ? false : true ;
+
+        items.forEach(item => {
+            if (item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState(
+            items
+        )
+
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} onClick= {(e => this.handleCardClick(item.id, e))} key={item.id}/>
+        })
+    }
+
+
+
     render (){
         return (
-            <div> Hola carrusel</div>
+            <Container fluid>
+                <Row className='justify-content-around'>
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
         )
     }
 
